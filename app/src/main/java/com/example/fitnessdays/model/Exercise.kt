842e90c773e8581182c28a15ec03191d7647afee
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-05-08 18:19:25 +0200

    Fix config

--- app/src/main/java/com/example/fitnessdays/model/Exercise.kt
+++ app/src/main/java/com/example/fitnessdays/model/Exercise.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class Exercise(
-    val dayNumber: Number,
+    val dayNumber: Int,
     @param:StringRes val titleRes: Int,
     @param:StringRes val descriptionRes: Int,
     @param:DrawableRes val imageRes: Int,
