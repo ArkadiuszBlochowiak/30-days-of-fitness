62ed303eb61a1b1d14e0d362f1a09f1d5606579c
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-05-08 20:24:08 +0200

    Hide description under expand action

--- app/src/main/java/com/example/fitnessdays/ExercisesScreen.kt
+++ app/src/main/java/com/example/fitnessdays/ExercisesScreen.kt
@@ -2,17 +2,32 @@ package com.example.fitnessdays
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
@@ -30,16 +45,29 @@ fun ExerciseItem(
     item: Exercise,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
+        onClick = { expanded = !expanded },
         modifier = modifier
     ) {
         Column(
             modifier = Modifier
-                .padding(dimensionResource(R.dimen.padding_medium))
+                .padding(
+                    horizontal = dimensionResource(R.dimen.padding_large),
+                    vertical = dimensionResource(R.dimen.padding_medium)
+                )
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
         ) {
             ExerciseItemHeader(
                 day = item.dayNumber,
                 title = item.titleRes,
+                expanded = expanded,
                 modifier = Modifier
                     .padding(
                         bottom = dimensionResource(R.dimen.padding_small)
@@ -52,9 +80,11 @@ fun ExerciseItem(
                         bottom = dimensionResource(R.dimen.padding_medium)
                     )
             )
-            ExerciseItemDescription(
-                description = item.descriptionRes
-            )
+            if(expanded) {
+                ExerciseItemDescription(
+                    description = item.descriptionRes
+                )
+            }
         }
     }
 }
@@ -63,16 +93,32 @@ fun ExerciseItem(
 fun ExerciseItemHeader(
     day: Int,
     @StringRes title: Int,
+    expanded: Boolean,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = stringResource(R.string.day, day),
-            style = MaterialTheme.typography.displaySmall
-        )
-        Text(
-            text = stringResource(title),
-            style = MaterialTheme.typography.displayLarge
+    val expandIcon = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Column(
+            modifier = Modifier
+        ) {
+            Text(
+                text = stringResource(R.string.day, day),
+                style = MaterialTheme.typography.displaySmall
+            )
+            Text(
+                text = stringResource(title),
+                style = MaterialTheme.typography.displayMedium
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            imageVector = expandIcon,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
         )
     }
 }
