9f1e4cab161f76e7555539c63e98343767a1b38e
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-05-08 18:42:41 +0200

    Display exercises app

--- app/src/main/java/com/example/fitnessdays/MainActivity.kt
+++ app/src/main/java/com/example/fitnessdays/MainActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.fitnessdays.data.ExercisesRepository
 import com.example.fitnessdays.ui.theme.FitnessDaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FitnessDaysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ExercisesApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ExercisesApp() {
+    val exercises = ExercisesRepository.exercises
+
+    Scaffold(
+        topBar = {
+            TopBarApp()
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        ExercisesList(
+            list = exercises,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    FitnessDaysTheme {
-        Greeting("Android")
-    }
+fun TopBarApp(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
 }
