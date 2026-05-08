2705e963ab97a1ce84231fc65364051c7e2e1813
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-05-08 15:33:50 +0200

    Create repository from data class

---
+++ app/src/main/java/com/example/fitnessdays/data/ExercisesRepository.kt
@@ -0,0 +1,39 @@
+package com.example.fitnessdays.data
+
+import com.example.fitnessdays.model.Exercise
+import com.example.fitnessdays.R
+
+object ExercisesRepository {
+    val exercises = listOf<Exercise>(
+        Exercise(1, R.string.day1_title, R.string.day1_description, R.drawable.day1),
+        Exercise(2, R.string.day2_title, R.string.day2_description, R.drawable.day2),
+        Exercise(3, R.string.day3_title, R.string.day3_description, R.drawable.day3),
+        Exercise(3, R.string.day4_title, R.string.day4_description, R.drawable.day4),
+        Exercise(3, R.string.day5_title, R.string.day5_description, R.drawable.day5),
+        Exercise(3, R.string.day6_title, R.string.day6_description, R.drawable.day6),
+        Exercise(3, R.string.day7_title, R.string.day7_description, R.drawable.day7),
+        Exercise(3, R.string.day8_title, R.string.day8_description, R.drawable.day8),
+        Exercise(3, R.string.day9_title, R.string.day9_description, R.drawable.day9),
+        Exercise(3, R.string.day10_title, R.string.day10_description, R.drawable.day10),
+        Exercise(3, R.string.day11_title, R.string.day11_description, R.drawable.day11),
+        Exercise(3, R.string.day12_title, R.string.day12_description, R.drawable.day12),
+        Exercise(3, R.string.day13_title, R.string.day13_description, R.drawable.day13),
+        Exercise(3, R.string.day14_title, R.string.day14_description, R.drawable.day14),
+        Exercise(3, R.string.day15_title, R.string.day15_description, R.drawable.day15),
+        Exercise(3, R.string.day16_title, R.string.day16_description, R.drawable.day16),
+        Exercise(3, R.string.day17_title, R.string.day17_description, R.drawable.day17),
+        Exercise(3, R.string.day18_title, R.string.day18_description, R.drawable.day18),
+        Exercise(3, R.string.day19_title, R.string.day19_description, R.drawable.day19),
+        Exercise(3, R.string.day20_title, R.string.day20_description, R.drawable.day20),
+        Exercise(3, R.string.day21_title, R.string.day21_description, R.drawable.day21),
+        Exercise(3, R.string.day22_title, R.string.day22_description, R.drawable.day22),
+        Exercise(3, R.string.day23_title, R.string.day23_description, R.drawable.day23),
+        Exercise(3, R.string.day24_title, R.string.day24_description, R.drawable.day24),
+        Exercise(3, R.string.day25_title, R.string.day25_description, R.drawable.day25),
+        Exercise(3, R.string.day26_title, R.string.day26_description, R.drawable.day26),
+        Exercise(3, R.string.day27_title, R.string.day27_description, R.drawable.day27),
+        Exercise(3, R.string.day28_title, R.string.day28_description, R.drawable.day28),
+        Exercise(3, R.string.day29_title, R.string.day29_description, R.drawable.day29),
+        Exercise(3, R.string.day30_title, R.string.day30_description, R.drawable.day30)
+    )
+}
